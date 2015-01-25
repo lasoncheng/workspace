b253a8ef072b1196162672ff1644dfacab0b6a66
lasoncheng <1126278338@qq.com>
2015-01-25 13:48:39 +0800

    update

---
+++ Test/src/BasicK/Person.java
@@ -0,0 +1,20 @@
+package BasicK;
+/**
+ * 类、超类、抽象类、接口、内部类、匿名类、外部类等
+ * @author lason
+ *
+ */
+
+abstract  public class Person {
+	private  String name ;
+	private  String sex;
+	private   int age;
+	void person(){};
+	abstract String getDescription();// abstract mehtod
+	
+}
+
+ 
+	
+	
+   
